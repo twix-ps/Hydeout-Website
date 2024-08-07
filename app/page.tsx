9edb96a46a1c25acdef165dc20a2664986a00ce5
ihyd <DerpsDer.es@gmail.com>
2024-08-08 00:50:57 +0300

    Removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 "use client"
-import {useState, useEffect, useRef, Suspense} from "react";
+import {Suspense} from "react";
 import Payments from "./components/ui/Payments";
 import SidePanel from "./components/ui/SidePanel";
 import {SessionProvider} from 'next-auth/react'
-import type {AppProps} from 'next/app'
-import { Button } from "@radix-ui/themes";
-import { DiscordLogoIcon } from "@radix-ui/react-icons";
-import {signIn, signOut, useSession} from 'next-auth/react'
-import { usePathname, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import Leaderboard from "./components/ui/Leaderboard";
 
 
