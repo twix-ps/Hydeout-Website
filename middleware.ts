8c1a7c6e7066cd07223efae793be9e4ff553d427
ihyd <DerpsDer.es@gmail.com>
2024-08-08 21:51:13 +0300

    Hotpatched Middleware, disabled CORs

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,3 @@
 import { NextResponse } from "next/server"
 
 export { default } from "next-auth/middleware"
-
-export function middleware() {
-    // retrieve the current response
-    const res = NextResponse.next()
-
-    // add the CORS headers to the response
-    res.headers.append('Access-Control-Allow-Credentials', "true")
-    res.headers.append('Access-Control-Allow-Origin', 'https://www.ihyd.xyz') // replace this your actual origin
-    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-    res.headers.append(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-
-    return res
-}
-
-// specify the path regex to apply the middleware to
-export const config = {
-    matcher: '/api/:path*',
-}
