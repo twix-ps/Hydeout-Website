d7e69bdb04ffeedfd80f2d56d3473579450ee927
twix <59828082+twix-ps@users.noreply.github.com>
2024-10-20 10:07:06 +0300

    A

--- app/api/side/route.ts
+++ app/api/side/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
-const message = "Error! Contact ihyd support.";
+const message = "Rustbet is down effective immediately, for more information join this server: https://discord.gg/TtkWDVMt8r and wait for an announcement!";
 
 export async function GET(req: Request) {
-    return NextResponse.json({ error: message }, { status: 200 });
+    return NextResponse.json({ error: message }, { status: 201 });
 }
